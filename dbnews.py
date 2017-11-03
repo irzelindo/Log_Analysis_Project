698e3e21833028139c6e239b2693de3a07d81010
irzelindo salvador <irzelindo.salvador@gmail.com>
2017-11-03 18:58:29 +0200

    Remove DB connection repetition

--- dbnews.py
+++ dbnews.py
@@ -3,49 +3,51 @@
 
 import psycopg2
 
-DBNAME = "news"
-
+def connect(DBNAME = "news"):
+    try:
+        db = psycopg2.connect("dbname={}".format(DBNAME))
+        cursor = db.cursor()
+        return db, cursor
+    except:
+        print("error trying to connect to DB")
 
 def question_one():
     """ Method Will return question #1 answer from database """
-    conn = psycopg2.connect(database=DBNAME)
-    cur = conn.cursor()
-    cur.execute("""select articles.title as article,
+    db, cursor = connect()
+    cursor.execute("""select articles.title as article,
                     count(*) as views from log,
                     articles where log.path
                     like(concat('/article/',articles.slug))
                     group by article order by views desc limit 3""")
-    answer_one = cur.fetchall()
+    answer_one = cursor.fetchall()
     return answer_one
-    conn.close()
-    cur.close()
+    db.close()
+    cursor.close()
 
 
 def question_two():
     """ Method Will return question #2 answer from database """
-    conn = psycopg2.connect(database=DBNAME)
-    cur = conn.cursor()
-    cur.execute("""select name, sum(views) as views from (select authors.name,
+    db, cursor = connect()
+    cursor.execute("""select name, sum(views) as views from (select authors.name,
                     articles.title, count(*) as views from log, articles,
                     authors where log.path like(concat('/article/',
                     articles.slug)) AND authors.id = articles.author
                     group by authors.name, articles.title order by views desc)
                     as article_views group by name order by views desc""")
-    answer_two = cur.fetchall()
+    answer_two = cursor.fetchall()
     return answer_two
-    conn.close()
-    cur.close()
+    db.close()
+    cursor.close()
 
 
 def question_three():
     """ Method Will return question #3 answer from database """
-    conn = psycopg2.connect(database=DBNAME)
-    cur = conn.cursor()
-    cur.execute("""select * from (select day,
+    db, cursor = connect()
+    cursor.execute("""select * from (select day,
                     round((round(errors,2)*100)/round(total,2),2)
                     as percentage from requests) as bad_day where
                     percentage>1""")
-    answer_three = cur.fetchall()
+    answer_three = cursor.fetchall()
     return answer_three
-    conn.close()
-    cur.close()
+    db.close()
+    cursor.close()
